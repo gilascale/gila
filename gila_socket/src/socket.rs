fb96062855b00555880f5e2e037c8a0a9ae177b3
James Clarke <jkc200013@gmail.com>
2024-12-20 18:13:52 +0000

    passing args works :)

--- gila_socket/src/socket.rs
+++ gila_socket/src/socket.rs
@@ -3,10 +3,12 @@ use gila::execution::ProcessContext;
 use gila::execution::SharedExecutionContext;
 
 #[no_mangle]
-pub fn create_socket(
+pub extern "C" fn create_socket(
     shared_execution_context: &mut SharedExecutionContext,
     execution_context: &mut ProcessContext,
     args: Vec<Object>,
 ) -> Object {
-    Object::I64(12)
+    let port = &args[0];
+    println!("create_socket port={:?}", port);
+    return Object::I64(12);
 }
