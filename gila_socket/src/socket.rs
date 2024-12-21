777fa397091945815cb62d6f09a2cc679138a59b
jkingstonc <j.kingston.c@gmail.com>
2024-12-21 13:35:43 +0000

    got listening working

--- gila_socket/src/socket.rs
+++ gila_socket/src/socket.rs
@@ -8,9 +8,14 @@ use windows::Win32::Networking::WinSock::{
 
 const AF_INET: c_int = 2;
 const SOCK_STREAM: c_int = 1;
+const INADDR_ANY: c_int = 0;
 
 fn makeword(low: u8, high: u8) -> u16 {
     ((high as u16) << 8) | (low as u16)
+}
+
+fn htons(host_short: u16) -> u16 {
+    host_short.to_be()
 }
 
 #[no_mangle]
@@ -57,8 +62,6 @@ pub extern "C" fn create_socket(
     args: Vec<Object>,
 ) -> Object {
     unsafe {
-        let port = &args[0].as_i64();
-
         let socket_fd = socket(AF_INET, SOCK_STREAM, 0);
         if socket_fd as i64 == -1 {
             // Check the errno for more detailed error information
@@ -71,15 +74,52 @@ pub extern "C" fn create_socket(
     }
 }
 
+#[repr(C)]
+#[derive(Debug, Clone, Copy)]
+struct sockaddr_in {
+    sin_family: u16,   // Address family (AF_INET)
+    sin_port: u16,     // Port number
+    sin_addr: u32,     // IPv4 address
+    sin_zero: [u8; 8], // Padding to match the size of `sockaddr`
+}
+
 #[no_mangle]
 pub extern "C" fn listen_socket(
     shared_execution_context: &mut SharedExecutionContext,
     execution_context: &mut ProcessContext,
     args: Vec<Object>,
 ) -> Object {
-    let port = &args[0];
-    println!("listen_socket port={:?}", port);
-    return Object::I64(12);
+    let socket = args[0].as_i64().unwrap() as c_int;
+    let port = args[0].as_i64().unwrap() as c_int;
+
+    // Create and populate the sockaddr_in struct
+    let mut addr = sockaddr_in {
+        sin_family: AF_INET as u16,
+        sin_port: htons(port as u16),
+        sin_addr: INADDR_ANY as u32,
+        sin_zero: [0; 8],
+    };
+
+    unsafe {
+        // Bind the socket to the port
+        if bind(
+            std::mem::size_of_val(&addr) as usize,
+            &addr as *const _ as *const sockaddr,
+            socket,
+        ) == -1
+        {
+            let err_code = std::io::Error::last_os_error().raw_os_error().unwrap();
+            panic!("Failed to bind socket, errno = {:?}", err_code);
+        }
+
+        // Start listening on the socket
+        if listen(socket.try_into().unwrap(), 5) == -1 {
+            let err_code = std::io::Error::last_os_error().raw_os_error().unwrap();
+            panic!("Failed to listen on socket, errno = {:?}", err_code);
+        }
+    }
+    println!("Socket is listening on port {}", port);
+    return Object::I64(0);
 }
 
 #[no_mangle]
