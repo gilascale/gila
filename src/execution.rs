fb881075cae788f44fa81b54c9faa4d9806d6e31
jkingstonc <j.kingston.c@gmail.com>
2024-12-25 17:58:29 +0000

    added 'add fn(self:Vec)...' to stdlib

--- src/execution.rs
+++ src/execution.rs
@@ -2238,9 +2238,10 @@ impl ExecutionEngine {
                 increment_ip!(self)
             }
             _ => {
+                println!("ummm {:?}", obj);
                 return Err(RuntimeError::INVALID_ACCESS(
                     "struct access should be accessing object".to_string(),
-                ))
+                ));
             }
         }
 
